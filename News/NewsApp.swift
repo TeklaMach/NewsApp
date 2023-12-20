98398146b8bdbe2227575f3c3b406dd48cb9a5fe
TeklaMach <macharashvilitekla@gmail.com>
2023-12-20 14:54:15 +0400

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Tekla Matcharashvili on 20.12.23.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
